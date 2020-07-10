681292cee4c8e1401a5df8eb10e4f089ccc99f75
mdmnislam <2020webproject@gmail.com>
2020-07-10 19:29:31 -0400

    Modified

--- app/app.py
+++ app/app.py
@@ -1,19 +1,13 @@
-# from typing import List, Dict
 import mysql.connector
-# import simplejson as json
-# import str as str
-# from flask import Flask, Response, request, redirect
 from flask import Flask, render_template
-
-app = Flask(__name__ , template_folder='templates')
-
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
+app = Flask(__name__ , template_folder='templates')
 mysql = MySQL(cursorclass=DictCursor)
 
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
-app.config['MYSQL_DATABASE_USER'] = 'afu2'
+app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'heightWeight'
@@ -22,7 +16,6 @@ mysql.init_app(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    # user = {'username': 'Height Weight Data'}
     cursor = mysql.get_db().cursor()
     cursor.execute('SELECT * FROM hw10')
     result = cursor.fetchall()
@@ -32,12 +25,9 @@ def index():
 @app.route('/view/<int:Index>', methods=['GET'])
 def view(Index):
     cursor = mysql.get_db().cursor()
-    # id = city_id.id
-    # id = request.args.get('city_id')
-    # id = request.form('city_id')
     cursor.execute('SELECT * FROM hw10 WHERE `Index`= %s', Index)
     result = cursor.fetchall()
-    return render_template('view.html', title='View Form', output=result[0])
+return render_template('view.html', title='View Form', output=result[0])
 
 
 if __name__ == "__main__":
